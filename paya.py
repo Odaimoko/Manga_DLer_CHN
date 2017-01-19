079f2e22ae0fc6bcb76ca057ae31a6cb0d2b54c4
Imoko Oda <Imoko Oda>
2017-01-19 18:53:01 +0800

    tryJson里是单线程，注释是多线程（失败），准备用Pool

--- paya.py
+++ paya.py
@@ -1,7 +1,10 @@
-from urllib import request, parse
+from urllib import request, parse, error
 import re, json
 import sys, time
+import threading, multiprocessing
+import multiprocessing.pool.Pool as Pool
 
+# multiprocessing.Queue
 # sys.path.append("..")
 # import iMyUtil
 
@@ -9,40 +12,54 @@ already_tenma_file = "already_tenma_json.txt"
 log_tenma_file = "tenma_log_json.txt"
 already_tenma = set()
 local_tenma = "天才麻将少女_本篇_json/"
+num_of_threads = 4
 
 
-try:
-	with open(already_tenma_file, "rb") as f:
-		already_text = f.read().decode("utf8")
-		for line in already_text.splitlines():
-			# 记录全部url，包括http:// 和末尾的/
-			already_tenma.add(line)
-		del already_text
-except FileNotFoundError as e:
-	# 未创建，创啊
-	with open(already_tenma_file, "w") as f:
-		pass
+def initializeAlready(alreadySet, alreadyFile):
+	try:
+		with open(alreadyFile, "rb") as f:
+			already_text = f.read().decode("utf8")
+			for line in already_text.splitlines():
+				# 记录全部url，包括http:// 和末尾的/
+				alreadySet.add(line)
+			del already_text
+	except FileNotFoundError as e:
+		# 未创建，创啊
+		with open(alreadyFile, "w") as f:
+			pass
+
+
+lock_already = threading.Lock()
+lock_log_file = threading.Lock()
 
 
 def addToAlready(content, alreadySet, alreadyFileName):
 	if content in alreadySet:
 		return
-	alreadySet.add(content)
-	with open(alreadyFileName, "ab") as al:
-		al.write(str(content).encode("utf8"))
-		al.write("\n".encode())
+	lock_already.acquire()
+	try:
+		alreadySet.add(content)
+		with open(alreadyFileName, "ab") as al:
+			al.write(str(content).encode("utf8"))
+			al.write("\n".encode())
+	finally:
+		lock_already.release()
 
 
 def record_log(logFileName, *src):
-	print(" ".join(src))
-	with open(logFileName, "ab") as lo:
-		# 解决直接write出现 encode 错误，还是手动encode+写入二进制
-		lo.write(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())).encode())
-		lo.write(":".encode())
-		for s in src:
-			lo.write(" ".encode())
-			lo.write(str(s).encode())
-		lo.write("\n".encode())
+	lock_log_file.acquire()
+	try:
+		print(" ".join(src))
+		with open(logFileName, "ab") as lo:
+			# 解决直接write出现 encode 错误，还是手动encode+写入二进制
+			lo.write(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())).encode())
+			lo.write(":".encode())
+			for s in src:
+				lo.write(" ".encode())
+				lo.write(str(s).encode())
+			lo.write("\n".encode())
+	finally:
+		lock_log_file.release()
 
 
 def createFolder(name):
@@ -60,6 +77,13 @@ def createFolder(name):
 
 def tema(ep_url, folder_name):
 	# 此处 addtoalready 是加入图片，编号为 #folder_name_#图片编号
+
+	# if True:
+	# 	print(folder_name, ep_url)
+	# 	res=request.urlopen("https://manhua.163.com/")
+	# 	print(res.read().decode("utf8")[:19].encode("utf8"))
+	# 	return
+
 	createFolder(folder_name)
 	# page_rq = request.Request("https://manhua.163.com/reader/4458002705630123103/")
 	page_rq = request.Request(ep_url)
@@ -74,13 +98,13 @@ def tema(ep_url, folder_name):
 		url = url[1:len(url) - 2]  # 得到地址（拿来用）
 		file_name = folder_name + '{:0>3}'.format(str(num)) + ".png"
 		if file_name in already_tenma:
-			record_log(log_tenma_file, file_name, "已下载")
+			# record_log(log_tenma_file, file_name, "已下载")
 			continue
 		try:
+			rq = request.Request(url)  # 也许有机会重用一个对象，因为这里每次都要新建一个
+			rq.add_header('User-Agent', 'Mozilla/4.0(compatible;MSIE5.5;WindowsNT)')
+			response = request.urlopen(rq, timeout=3000)
 			with open(file_name, "wb") as pic:
-				rq = request.Request(url)  # 也许有机会重用一个对象，因为这里每次都要新建一个
-				rq.add_header('User-Agent', 'Mozilla/4.0(compatible;MSIE5.5;WindowsNT)')
-				response = request.urlopen(rq, timeout=3000)
 				pic.write(response.read())
 				addToAlready(file_name, already_tenma, already_tenma_file)
 		except Exception as e:
@@ -89,7 +113,10 @@ def tema(ep_url, folder_name):
 	record_log(log_tenma_file, folder_name, "下载完成")
 
 
+# threading.current_thread().join()
+
 def TEMA():
+	# Deprecated: Using episode urls saved locally
 	eps = []
 	with open("tenma_downloadMenu.txt", "r") as f:
 		for line in f.read().splitlines():
@@ -104,6 +131,20 @@ def TEMA():
 
 
 def try_getJson():
+	# threads = [threading.Thread() for i in range(num_of_threads)]
+	#
+	# # using_set=set()
+	# # unused_set = set([th for th in threads])
+	#
+	# def getThreadUnused():
+	# 	print("alive? ",[th.is_alive() for th in threads])
+	# 	for th in threads:
+	# 		if not th.is_alive():
+	# 			return th
+	# 	return None
+	# for th in threads:
+	# 	th.start()
+
 	json_url = "https://manhua.163.com/book/catalog/4458002705630123103.json"
 	rq = request.Request(json_url)
 	response = request.urlopen(rq)
@@ -114,7 +155,7 @@ def try_getJson():
 	num_of_ep = 0
 	for each in js["catalog"]["sections"][0]["sections"]:
 		num_of_ep += 1
-		# if(num_of_ep>3):
+		# if(num_of_ep>5):
 		# 	print("say ggooooooddbyeee")
 		# 	break
 		bookId = each["bookId"]
@@ -124,8 +165,32 @@ def try_getJson():
 		url_one_wa = "https://manhua.163.com/reader/" + bookId + "/" + sectionId + "/"
 		ep_folder_name = local_tenma + '{:0>4}'.format(str(num_of_ep)) + " " + fullTitle + '/'  # 格式化文件夹名字，用0补全前面
 		tema(url_one_wa, ep_folder_name)
-		# print(ep_folder_name)
+	# print(ep_folder_name)
+	# print(len(unused_set))
+	# 	try:
+	# 		# next_thread = unused_set.pop()
+	# 		next_thread = getThreadUnused()
+	# 		print(next_thread.name)
+	#
+	# 		next_thread._target = tema
+	# 		next_thread._args = (url_one_wa, ep_folder_name)
+	# 		next_thread._kwargs=dict()
+	# 		record_log("开始下载", ep_folder_name)
+	# 		next_thread.run()
+	# 		# next_thread.join()
+	# 	# unused_set.add(next_thread)
+	# 	except KeyError as k:
+	# 		# 所有线程都在运行
+	# 		print("火鹤")
+	#
+	# 		# for th in threads:
+	# 		# 	th.join()
+	# 		# unused_set = set([th for th in threads])
+	#
+	# print(threading.enumerate())
 
-# TEMA()
 
+# 使用多线程： 用每个线程下载不同的图片（每一话新开pool）
+initializeAlready(already_tenma, already_tenma_file)
 try_getJson()
+tenma = "4458002705630123103"
